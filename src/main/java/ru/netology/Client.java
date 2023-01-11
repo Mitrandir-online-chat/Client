778437a7111414c69c45caa559a3d3e463dfdb83
Илья Грачев <grachev.ia96@gmail.com>
2023-01-12 00:03:51 +0700

    prepared Client for connecting

--- src/main/java/ru/netology/Client.java
+++ src/main/java/ru/netology/Client.java
@@ -1,12 +1,33 @@
 package ru.netology;
 
+import org.yaml.snakeyaml.Yaml;
 import ru.netology.controller.Controller;
 import ru.netology.repository.Repository;
 import ru.netology.service.Service;
+import ru.netology.settings.Settings;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         createLayers();
+
+        var settings = getSettings();
+        var address = settings.getAddress();
+        var port = settings.getPort();
+
+        try (var clientSocket = new Socket(address, Integer.parseInt(port))) {
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+
+
 
     }
 
@@ -14,5 +35,14 @@ public class Client {
         var repository = new Repository();
         var service = new Service(repository);
         var controller = new Controller(service);
+    }
+
+    private static Settings getSettings() throws IOException {
+        // TODO надо убрать _dev
+        var path = "settings_dev.yml";
+        var yaml = new Yaml();
+        var in = Files.newInputStream(Paths.get(path));
+        Settings settings = yaml.load(in);
+        return settings;
     }
 }
