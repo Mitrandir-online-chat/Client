72114deabff73013abad29b5703acf07430bb298
Илья Грачев <grachev.ia96@gmail.com>
2023-01-11 23:45:46 +0700

    added method of layers initialization

--- src/main/java/ru/netology/Client.java
+++ src/main/java/ru/netology/Client.java
@@ -1,7 +1,18 @@
 package ru.netology;
 
+import ru.netology.controller.Controller;
+import ru.netology.repository.Repository;
+import ru.netology.service.Service;
+
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        createLayers();
+
+    }
+
+    private static void createLayers() {
+        var repository = new Repository();
+        var service = new Service(repository);
+        var controller = new Controller(service);
     }
 }
